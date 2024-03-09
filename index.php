cc4c124456aa54ac320a0897ed6a4050078c53eb
Conrad Christian <conradchristian812@gmail.com>
2024-03-09 02:32:58 -0800

    creation of posts

--- index.php
+++ index.php
@@ -1,35 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./src/css/reset.css">
     <link rel="stylesheet" href="./src/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="BCIT Student Social Media">
     <meta name="keywords" content="BCIT, Student, Social, Media">
     <title>Student Social Media</title>
 </head>
+
 <body>
     <div class="wrap">
         <!-- Navbar -->
-        <?php $title = 'Student Social Media'?>
-        <?php 
-        include("src/components/header.php"); 
+        <?php $title = 'Student Social Media' ?>
+        <?php
+        include("src/components/header.php");
         include("./include_db.php");  // Connects the the db
         include("src/database/initalize.php");  // Initalizes the db
 
+        // session user id
+        // $id = $_SESSION['id'];
+        ?>
+
+        <form action="posts/create_post.php" method="post">
+            <div>
+                <label for="title">Title: </label>
+                <input for="title" name="title" id="title" />
+            </div>
+
+            <div>
+                <label for="post">Post: </label>
+                <input for="post" name="post" id="post" />
+            </div>
+
+            <!-- When session is implemented use this, otherwise test user Kim, ID = 1 -->
+            <!-- <input type="hidden" name="id" value="<?php //echo $id; 
+                                                        ?>"> -->
+            <input type="hidden" name="id" value=1>
+
+            <div>
+                <input type="submit" value="create" name="create" />
+            </div>
+        </form>
+
+        <?php
         // Prints the table
         $res = $db->query('SELECT * FROM posts');
 
         // TODO: Made a table, going to need to fix front end to match figma
         $count = 1;
         while ($row = $res->fetchArray()) {
-        echo "<h1>Post $count</h1>\n";
-        echo "<table>\n";
-        echo "<tr><th>User</th>".
-                "<th>Title</th>".
-                "<th>Post</th>".
+            echo "<h1>Post $count</h1>\n";
+            echo "<table>\n";
+            echo "<tr><th>User</th>" .
+                "<th>Title</th>" .
+                "<th>Post</th>" .
                 "<th>Likes</th>" .
                 "<th>Date</th></tr>\n";
             echo "<tr>";
@@ -44,14 +72,15 @@
             echo "<a href='/posts/display_post.php?id={$row['0']}'>View More</a>";
 
 
-            $count++; 
+            $count++;
         };
 
         ?>
-        
+
         <?php include("src/components/footer.php"); ?>
     </div>
 
     <script src="./src/js/app.js"></script>
 </body>
+
 </html>
--- posts/create_post.php
+++ posts/create_post.php
@@ -1,26 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="/../src/css/reset.css">
     <link rel="stylesheet" href="/../src/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="BCIT Student Social Media">
     <meta name="keywords" content="BCIT, Student, Social, Media">
     <title>Creating Post</title>
 </head>
+
 <body>
     <div class="wrap">
         <!-- Navbar -->
-        <?php $title = 'Create Post';?>
+        <?php $title = 'Create Post'; ?>
         <?php include(__DIR__ . "/../src/components/header.php"); ?>
 
-        
+        <?php
+        include("./../include_db.php");
+        include("validate_functions.php");
+
+        if (isset($_POST['create'])) {
+            
+            // Getting the data from the form
+            extract($_POST);
+
+            // Sanitizing the post data
+            $post = sanitize_input($post);
+            $title = sanitize_input($title);
+
+            // If one of the fields is empty, redirect to index
+            if (empty($title) || empty($post)) {
+                header('Location: /../index.php');
+                exit;
+            }
+
+            // Find user information from db
+            $stm = $db->prepare('SELECT * FROM users WHERE ID = :id');
+            $stm->bindValue(':id', "$id", SQLITE3_TEXT);
+            $res = $stm->execute();
+
+            $row = $res->fetchArray(SQLITE3_NUM);
+            if ($row === false) {
+                header('Location: /../index.php');
+                exit;
+            }
+            $userid = $row[0];
+            $name = $row[1];
+
+            $SQL_insert_data = "INSERT INTO posts (UserName, UserID, title, post, likes, date)
+            VALUES ('$name', '$userid', '$title', '$post', 0, date('now'))";
+
+            $db->exec($SQL_insert_data);
+            $changes = $db->changes();
+
+            header('Location: ../../index.php');
+            exit;
+        }
+        ?>
+
         <!-- Main -->
         <?php include(__DIR__ . "/../src/components/footer.php"); ?>
     </div>
 
     <script src="../src/js/app.js"></script>
 </body>
+
 </html>
