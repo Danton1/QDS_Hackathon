022ed7e8392f646bbde9da793c10ae6256295c15
Alfrey Chan <alfreychan@gmail.com>
2024-03-09 17:38:20 -0800

    Fix file paths

--- login.php
+++ login.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./src/css/reset.css">
     <link rel="stylesheet" href="./src/css/login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">      
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="BCIT Student Social Media">
     <meta name="keywords" content="BCIT, Student, Social, Media">
@@ -14,11 +15,17 @@
 
     <div class="wrap">
         <!-- Navbar -->
-        <?php 
-            $title = 'Login';
-            define('BYPASS_AUTH', true);
-            include("config_session.php");
-            include("src/components/header.php"); 
+        <?php
+            if (isset($_SESSION['success'])) {
+                $success = $_SESSION['success'];
+                        
+                $display_success = "<div class='alert alert-success'>"
+                . "<p>" . htmlspecialchars($success) . "</p>"
+                . "</div>";
+
+                echo $display_success;
+                unset($_SESSION['success']);
+            } 
         ?>
 
         <!-- Main -->
