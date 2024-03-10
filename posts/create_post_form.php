e6bcc1ab432056963f89274d66018bf524fef0bd
sunw00000 <sunwooo0728@gmail.com>
2024-03-09 22:05:17 -0800

    Fixed the top button on index page

--- posts/create_post_form.php
+++ posts/create_post_form.php
@@ -57,7 +57,6 @@ $program = $row['ProgramName'];
         <option value="2">2</option>
         <option value="3">3</option>
         <option value="4">4</option>
-        <option value="5">5</option>
         <option value="co-op">Co-op</option>
     </select>
     <div id="courseListing"></div>
