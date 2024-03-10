5669de3e2da1f701becbf876a056ee4169fb3b0b
Alfrey Chan <alfreychan@gmail.com>
2024-03-09 18:16:27 -0800

    Remove overriding bootstrap styling

--- signup.php
+++ signup.php
@@ -11,7 +11,6 @@ require_once('include_db.php');
     <link rel="stylesheet" href="./src/css/reset.css">
     <link rel="stylesheet" href="./src/css/signup.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="BCIT Student Social Media">
     <meta name="keywords" content="BCIT, Student, Social, Media">
@@ -29,16 +28,9 @@ require_once('include_db.php');
             <div class="form-container">
                 <?php
                     if (isset($_SESSION['errors'])) {
-                        $errors = $_SESSION['errors'];
-                        
-                        $display_errors = "<div class='alert alert-danger'>";
-
-                        foreach ($errors as $error) {
-                            $display_errors .= "<p>" . htmlspecialchars($error) . "</p>";
+                        foreach ($_SESSION['errors'] as $error) {
+                            echo "<p class='error'>$error</p>";
                         }
-                        $display_errors .= "</div>";
-
-                        echo $display_errors;
                         unset($_SESSION['errors']);
                     } 
                 ?>
